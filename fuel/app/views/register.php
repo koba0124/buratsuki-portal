218fa7c9b20a9dfcbd30c59ea5927a6d6770f99e
Kazuki Asakura <32762324+Arthur1@users.noreply.github.com>
2019-03-07 02:23:11 +0900

    フォームのクリック範囲を修正

--- fuel/app/views/register.php
+++ fuel/app/views/register.php
@@ -9,24 +9,24 @@
 		</div>
 		<div class="col s12 l7 input-field">
 			<?= Form::input('screen_name', Input::post('screen_name'), ['required' => true, 'class' => $classes['screen_name']]); ?>
-			<?= Form::label('表示名', 'form_screen_name'); ?>
+			<?= Form::label('表示名', 'screen_name'); ?>
 		</div>
 		<div class="col s12 l7 input-field">
 
 			<?= Form::input('email', Input::post('email'), ['type' => 'email', 'required' => true, 'class' => $classes['email']]); ?>
-			<?= Form::label('メールアドレス', 'form_email'); ?>
+			<?= Form::label('メールアドレス', 'email'); ?>
 		</div>
 		<div class="col s12 l7 input-field">
 			<?= Form::password('password', Input::post('password'), ['required' => true, 'class' => $classes['password']]); ?>
-			<?= Form::label('パスワード', 'form_password'); ?>
+			<?= Form::label('パスワード', 'password'); ?>
 		</div>
 		<div class="col s12 l7 input-field">
 			<?= Form::password('password_check', Input::post('password_check'), ['required' => true, 'class' => $classes['password_check']]); ?>
-			<?= Form::label('パスワード(確認)', 'form_password_check'); ?>
+			<?= Form::label('パスワード(確認)', 'password_check'); ?>
 		</div>
 		<div class="col s12 l7 input-field">
 			<?= Form::input('watchword', Input::post('watchword'), ['required' => true, 'class' => $classes['watchword']]); ?>
-			<?= Form::label('合言葉', 'form_watchword'); ?>
+			<?= Form::label('合言葉', 'watchword'); ?>
 		</div>
 		<div class="col s12 input-field">
 			<?= Form::submit('submit', '登録', ['class' => 'btn teal']); ?>
