15b9c9b2fda017767f8731070335f39412e3a964
koba0124 <23511389+koba0124@users.noreply.github.com>
2020-03-29 22:39:43 +0900

    身内向けサイト用に記載を変更・Google Analyticsを削除

--- fuel/app/views/about.php
+++ fuel/app/views/about.php
@@ -1,13 +1,13 @@
 <div class="container">
 	<p>
-		「ぶらつき学生ポータル」は、<a href="https://twitter.com/aaron_teleneco" target="_blank" rel="noopener">Arthur</a>がオープンソースで開発しているアグリコラ戦績管理ツールです。旧版EIK・リバイズドはもちろん、基本的な拡張デッキや「泥沼からの出発」拡張にも対応しています。
+		「ほら吹き社会人ポータル」は、<a href="https://twitter.com/aaron_teleneco" target="_blank" rel="noopener">Arthur</a> 様がオープンソースで開発されているアグリコラ戦績管理ツールです。旧版EIK・リバイズドはもちろん、基本的な拡張デッキや「泥沼からの出発」拡張にも対応しています。
 	</p>
 	<p>
-		本サイトは東京工業大学アグリコラサークル「ぶらつき学生連盟」用に開発されていますが、他のコミュニティでも運用できるような設計になっています。もし、本サイトのような戦績管理ツールを導入したい、というコミュニティの方がいらっしゃいましたら、Arthurまでお問い合わせください。
+		本サイトはArthur様のGithubソースを元にリリースされていますが、ソースから分析・構築したものであり、Arthur様と直接関係はないため、本家の内容についてのご質問には答えかねます。
 	</p>
 	<h2 class="orange-text" id="browser">推奨環境</h2>
 	<p>
-		「ぶらつき学生ポータル」を快適にご利用いただくために、以下のモダンブラウザ最新版のご利用を推奨します。
+		「ほら吹き社会人ポータル」を快適にご利用いただくために、以下のモダンブラウザ最新版のご利用を推奨します。
 	</p>
 	<ul class="browser-default">
 		<li>Google Chrome</li>
@@ -19,19 +19,9 @@
 		また、全ての機能をお使いいただくために、JavaScript、Cookieが使用できる状態でご利用ください。
 	</p>
 	<h2 class="orange-text" id="termsofuse">利用規約</h2>
-	<h4 class="teal-text">Google Analytics利用について</h4>
-	<p>
-		「ぶらつき学生ポータル」（以下、本サイト）では、より良いツール開発のためにGoogle Analyticsを使用しております。Googleに特定の情報（本サイトでアクセスしたページのURLやIPアドレスなど）を、個人を特定できない形で自動的に送信しています。
-	</p>
-	<p>
-		ブラウザ設定よりCookieの使用をオフにすることで、これを拒否することも可能です。ただし、本サイトの一部機能（認証機能など）が使用できなくなります。
-	</p>
-	<p>
-		また、Googleのアクセス情報の収集・利用方法は、<a href="https://www.google.com/analytics/terms/jp.html" rel="noopener" target="_blank">Google Analyticsサービス利用規約</a>および<a href="https://policies.google.com/privacy?hl=ja&gl=jp" rel="noopener" target="_blank">Googleポリシーと規約</a>によって定められています。
-	</p>
 	<h4 class="teal-text">個人情報について</h4>
 	<p>
-		メンバー登録時に収集した個人情報の使用は、本サイトおよび「ぶらつき学生連盟」が運営する別サービスの認証に限り、その他の目的で使用することは一切ありません。
+		メンバー登録時に収集した個人情報の使用は、本サイトの認証に限り、その他の目的で使用することは一切ありません。
 	</p>
 	<h4 class="teal-text">免責事項</h4>
 	<p>
