d59cd9a0581cf23bf80f0d3ff106801b0dd0e03a
koba0124 <23511389+koba0124@users.noreply.github.com>
2020-04-04 03:36:38 +0900

    Table Top Simulator

---
+++ fuel/app/views/tts.php
@@ -0,0 +1,83 @@
+<div class="container">
+	<h2 class="orange-text" id="browser">導入</h2>
+	<h3>Table Top Simulator</h3>
+	<p>
+		Steamで、下記のソフトを購入してください。<br>
+		<a href="https://store.steampowered.com/app/286160/Tabletop_Simulator/?l=japanese" target="_blank" rel="noopener">Table Top Simulator</a>
+	</p>
+
+	<h3>Agricola (Revised Edition, scripted)</h3>
+	<p>
+		Steam Communityで、下記のワークショップをサブスクライブしてください。<br>
+		<a href="https://steamcommunity.com/sharedfiles/filedetails/?id=2046438707" target="_blank" rel="noopener">Agricola Customized</a><br>
+		下記のオリジナルにカスタマイズを加えたものです。<br><br>
+		<a href="https://steamcommunity.com/sharedfiles/filedetails/?id=1931752711" target="_blank" rel="noopener">Agricola (Revised Edition, scripted)</a>
+	</p>
+
+	<h2 class="orange-text" id="browser">操作方法</h2>
+	<h3>やっちゃダメなこと</h3>
+	<p>
+		画面上部の「Flip」をクリック→ちゃぶ台返しです。<br>
+		CREATEした本人は Option > Permissions から、禁止することができますが、<br>
+		CREATEした本人の操作は禁止されないので気を付けてください。
+	</p>
+	<h3>ゲーム開始</h3>
+	<p>
+		<ul>
+			<li>だれか1人がCREATE (Server NameとPasswordを設定して、みんなに共有）</li>
+			<li>残りのメンバーはJOIN</li>
+		</ul>
+	</p>
+	<h3>場決め</h3>
+	<p>
+		<ul>
+			<li>決め方はあとでカスタマイズします（トランプ6枚から引くとか？）</li>
+			<li>場所が決まったら、右上のプレイヤー名からChange Colorで位置に合わせて座る</li>
+		</ul>
+	</p>
+	<h3>ドラフト</h3>
+	<p>
+		暫定案。たぶんこれでいけるとおもう。
+		<ul>
+			<li>START GAMEはまだクリックしない（クリックするとラウンド1がめくられてしまう</li>
+			<li>デッキの上で左クリック長押しをすると、デッキ全体をつかむことができる</li>
+			<li>デッキを重ねると、デッキがまとまる。まとまらない場合は、D&Dで選択してから「G」でまとまる</li>
+			<li>デッキを右クリック＞Shuffle でシャッフルする</li>
+			<li>デッキを右クリック＞Cut に合わせると右にゲージが表示される。<br>
+			そこで数字を選んでからCutすると、その枚数で裏向きに分割される</li>
+		</ul>
+		<ul>
+			<li>上記を駆使して、進歩と職業10枚ずつ、合わせて20枚の裏向きのデッキを作る</li>
+			<li>デッキを右クリック＞Search で、そのデッキの中身が右側に表示される。欲しいものを手札に入れる。</li>
+			<li>残りを次の人に回す。次の人はまたデッキ＞Search で選んでいく</li>
+			<li>手札のカードを確認するには[Alt]を押す</li>
+		</ul>
+	</p>
+	<h3>ゲーム進行</h3>
+		<ul>
+			<li>START GAMEでラウンド1が始まる</li>
+			<li>NEXT ROUNDで次のラウンドに移行する（累積の物は自動で置いてくれる。家族は帰ってくる）</li>
+			<li>END GAMEで点数計算(一部は自動計算してくれる？どこまで正しいかは不明)</li>
+		</ul>
+
+	<h3>主な操作</h3>
+		<ul>
+			<li>左クリックからのドラッグで1つ移動。左クリックしたまましばらく待っているとスタック全て移動</li>
+			<li>左クリックでドラッグ状態で右クリック、もしくは左クリックのドラッグで領域選択して「G」でグループ化</li>
+			<li>「W」「A」「S」「D」で視点を上下左右に移動</li>
+			<li>「Z」でズーム、スペースで視点をリセット</li>
+			<li>右クリックからのドラッグで視点の角度を変更</li>
+			<li>「Q」「E」もしくはマウスホイールで物体の角度を変更（柵など）</li>
+			<li>「Alt」でカードを拡大</li>
+			<li>「F」でカードを裏返す</li>
+			<li>他に必要な操作ある…？</li>
+		</ul>
+	<h3>参考リンク</h3>
+		<a href="https://w.atwiki.jp/ltts/pages/15.html" target="_blank" rel="noopener">基本操作</a><br>
+		<a href="https://w.atwiki.jp/ltts/pages/38.html" target="_blank" rel="noopener">詳細操作</a>
+
+	<h2 class="orange-text" id="browser">カスタマイズ</h2>
+	<p>
+		<a href="http://beginnerboardgames.hatenablog.com/entry/Tabeletop_Simulator" target="_blank" rel="noopener">Tabeletop Simulator MOD/ワークショップ/custom/自作解説講座/マルチ募集</a>
+	</p>
+</div>
